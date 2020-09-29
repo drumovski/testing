005619ff10a5b5c2fec30451bb99469c6a09e450
drumovski <jason.stacy@gmail.com>
2020-09-29 13:56:09 +1000

    add repeatMessage function

---
+++ source/index.js
@@ -0,0 +1,13 @@
+function repeatMessage(message, repeatCount){
+    let messageToShow = "";
+    for (let index = 0; index < repeatCount; index++) {
+        messageToShow += message; 
+    }
+    return messageToShow;
+}
+
+console.log(repeatMessage("hello", 3))
+
+module.exports = {
+    repeatMessage
+}
